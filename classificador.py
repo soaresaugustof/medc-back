c2af98a875b5f928dce3f4068ed52c48223dd830
Augusto Soares <soaresaugusto@edu.unifor.br>
2025-03-13 11:00:48 -0300

    Update classificador.py

--- classificador.py
+++ classificador.py
@@ -64,7 +64,7 @@ def weighted_loss(pos_weights, neg_weights, epsilon=1e-7):
     return loss
 
 
-img = preprocess_image_with_generator('cardiom3.png', target_size=(320, 320))
+# img = preprocess_image_with_generator('cardiom3.png', target_size=(320, 320))
 
 model = load_model('2409_2004i.h5', custom_objects = {'loss': weighted_loss})
 
