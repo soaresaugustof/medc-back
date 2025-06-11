add4d5862b9965d7699a9a77605a95d8e88bcef3
soaresaugustof <soaresaugusto@edu.unifor.br>
2025-06-10 23:38:26 -0300

    Refactor app.run to use environment variable for port configuration

--- main.py
+++ main.py
@@ -276,4 +276,7 @@ def get_exames():
         mydb.close()
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
